57e3844504fb7681e49e70124a533a0c7122072d
rokhayaSeck2003 <rokhayaseck038@gmail.com>
2026-07-11 15:03:15 +0000

    update premium

--- services/amadeus.service.js
+++ services/amadeus.service.js
@@ -16,38 +16,66 @@ export async function getAmadeusToken() {
         return amadeusToken;
     }
 
-    const response = await fetch(
-        "https://test.api.amadeus.com/v1/security/oauth2/token",
-        {
-            method: "POST",
+    try {
 
-            headers: {
-                "Content-Type":
-                    "application/x-www-form-urlencoded"
-            },
+        const response = await fetch(
 
-            body:
-`grant_type=client_credentials&client_id=${process.env.AMADEUS_API_KEY}&client_secret=${process.env.AMADEUS_API_SECRET}`
+            "https://api.amadeus.com/v1/security/oauth2/token",
+
+            {
+                method: "POST",
+
+                headers: {
+                    "Content-Type":
+                        "application/x-www-form-urlencoded"
+                },
+
+                body: new URLSearchParams({
+
+                    grant_type: "client_credentials",
+
+                    client_id:
+                        process.env.AMADEUS_API_KEY,
+
+                    client_secret:
+                        process.env.AMADEUS_API_SECRET
+
+                })
+
+            }
+
+        );
+
+        const data = await response.json();
+
+        console.log("AMADEUS TOKEN RESPONSE:", data);
+
+        if (!data.access_token) {
+
+            throw new Error(
+                JSON.stringify(data)
+            );
+
         }
-    );
 
-    const data = await response.json();
+        amadeusToken = data.access_token;
 
-    console.log("AMADEUS TOKEN RESPONSE :", data);
+        amadeusExpire =
+            Date.now() +
+            (data.expires_in * 1000);
 
-if (!data.access_token) {
-    throw new Error(JSON.stringify(data));
-}
+        console.log("✅ AMADEUS TOKEN OK");
 
-    amadeusToken = data.access_token;
+        return amadeusToken;
 
-    amadeusExpire =
-        Date.now() +
-        (data.expires_in - 60) * 1000;
+    } catch (err) {
 
-    console.log("✅ Nouveau token Amadeus");
+        console.log("❌ AMADEUS AUTH ERROR");
+        console.log(err);
 
-    return amadeusToken;
+        throw err;
+
+    }
 
 }
 // ======================================
